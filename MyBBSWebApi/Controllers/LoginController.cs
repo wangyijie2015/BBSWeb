e6ac35f972fd0ca1947dcc4f89b0dc225e16b0d7
韦 <1126146235@qq.com>
2022-07-03 23:11:33 +0800

    添加项目文件。

---
+++ MyBBSWebApi/Controllers/LoginController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyBBSWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    //[Route("[controller]")]  //restful 风格
+    [ApiController]
+    public class LoginController : ControllerBase
+    {
+    }
+}
