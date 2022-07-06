1276103378897a49f4b371101c041959d06a0fa7
MoRanGu <1126146235@qq.com>
2022-07-06 23:41:03 +0800

    FIx(Users.cs): 添加了 三个字段

--- MyBBSWebApi2.Models/Users.cs
+++ MyBBSWebApi2.Models/Users.cs
@@ -10,5 +10,8 @@ namespace MyBBSWebApi.Models
         public int UserLevel { get; set; }
         public string Password { get; set; }
         public bool IsDelete { get; set; }
+        public Guid Token { get; set; } // token 
+        public Guid AutoLoginTag { get; set; } // 自动登录的标识
+        public DateTime? AutoLoginLimitTime { get; set; }//自动登录的限制时间，可以设置什么时候失效
     }
 }
