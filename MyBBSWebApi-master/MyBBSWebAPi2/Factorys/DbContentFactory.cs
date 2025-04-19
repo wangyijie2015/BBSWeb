3d283d8690a928a52a1ab8c5f690d72fa2ff8e71
wangyijie2020 <1243097542@qq.com>
2025-04-19 23:35:58 +0800

    工厂模式

---
+++ MyBBSWebApi-master/MyBBSWebAPi2/Factorys/DbContentFactory.cs
@@ -0,0 +1,19 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//namespace MyBBSWebAPi.Factorys
+//{
+//    public class DbContentFactory
+//    {
+//        private DbContext _dbContext = null;
+
+//        public DbContext GetDbContext()
+//        {
+//            if(_dbContext == null)
+//            {
+//                _dbContext = new DbContext();
+//                _dbContext.Database.EnsureCreated();
+//            }
+//        }
+//    }
+
+
+//}
